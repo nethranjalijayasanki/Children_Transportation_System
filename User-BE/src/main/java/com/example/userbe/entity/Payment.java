3b9617f5f91bd49a74e7015cfc67cd61ee30c62e
nethranjalijayasanki <ssunflower620@gmail.com>
2025-04-24 10:05:25 +0530

    Add Payment Entity

---
+++ User-BE/src/main/java/com/example/userbe/entity/Payment.java
@@ -0,0 +1,26 @@
+package com.example.userbe.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+@Getter
+@Setter
+@Entity
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+    private LocalDate paymentDate;
+    private String paymentMethod;
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "passenger_id")
+    private Passenger passenger;
+}
