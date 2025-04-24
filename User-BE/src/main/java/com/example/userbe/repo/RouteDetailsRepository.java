51580a6d04a66f1debe21306da55066a943590f7
nethranjalijayasanki <ssunflower620@gmail.com>
2025-04-24 10:12:11 +0530

    Add Route Details Repository

---
+++ User-BE/src/main/java/com/example/userbe/repo/RouteDetailsRepository.java
@@ -0,0 +1,7 @@
+package com.example.userbe.repo;
+
+import com.example.userbe.entity.RouteDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RouteDetailsRepository extends JpaRepository<RouteDetails, Long> {
+}
