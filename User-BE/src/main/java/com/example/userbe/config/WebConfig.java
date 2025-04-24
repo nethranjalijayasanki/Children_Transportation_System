537501258ccb0a3b39cbdaf0395d4b38c9f3d78d
nethranjalijayasanki <ssunflower620@gmail.com>
2025-04-24 20:31:34 +0530

    add web config

--- User-BE/src/main/java/com/example/userbe/config/WebConfig.java
+++ User-BE/src/main/java/com/example/userbe/config/WebConfig.java
@@ -1,4 +1,4 @@
-/*
+
 package com.example.userbe.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,11 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/v1/**")
                 .allowedOrigins("http://localhost:8080") // Update with your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
-*/
+
