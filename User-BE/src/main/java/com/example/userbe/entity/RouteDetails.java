bd9f96ad9045f809e318fbb3d618a0c8fdbe045d
nethranjalijayasanki <ssunflower620@gmail.com>
2025-04-24 10:06:13 +0530

    Add Route Details Entity

---
+++ User-BE/src/main/java/com/example/userbe/entity/RouteDetails.java
@@ -0,0 +1,36 @@
+package com.example.userbe.entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "route_details")
+public class RouteDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String routeName;
+    private String startPoint;
+    private String endPoint;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id")
+    private Vehicle vehicle;
+
+    @ManyToMany
+    @JoinTable(
+            name = "route_passengers",
+            joinColumns = @JoinColumn(name = "route_id"),
+            inverseJoinColumns = @JoinColumn(name = "passenger_id")
+    )
+    private List<Passenger> passengers;
+
+}
