0e6c8ee6a8f8ea7b49a50b4bea15f3c587e5c606
nethranjalijayasanki <ssunflower620@gmail.com>
2025-04-24 10:31:49 +0530

    update driver DTO

--- User-BE/src/main/java/com/example/userbe/dto/DriverDTO.java
+++ User-BE/src/main/java/com/example/userbe/dto/DriverDTO.java
@@ -1,43 +1,16 @@
 package com.example.userbe.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class DriverDTO {
     private String name;
     private String licenseNumber;
     private String contactNumber;
 
-    // Constructors
-    public DriverDTO() {}
 
-    public DriverDTO( String name, String licenseNumber, String contactNumber) {
-
-        this.name = name;
-        this.licenseNumber = licenseNumber;
-        this.contactNumber = contactNumber;
-    }
-
-    // Getters and Setters
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLicenseNumber() {
-        return licenseNumber;
-    }
-
-    public void setLicenseNumber(String licenseNumber) {
-        this.licenseNumber = licenseNumber;
-    }
-
-    public String getContactNumber() {
-        return contactNumber;
-    }
-
-    public void setContactNumber(String contactNumber) {
-        this.contactNumber = contactNumber;
-    }
 }
